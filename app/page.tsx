9fdeca510fefe35392b5f060b99957710cc2e10d
miaalvv <mia.alvarez03@student.csulb.edu>
2025-09-17 15:52:01 -0700

    Update page.tsx
    testing deploymentt editables

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
-}
+
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -50,7 +48,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Start building
           </a>
         </div>
       </main>
